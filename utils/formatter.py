90f224cff4a2e80b676871ecfaa5c0749607b15e
Casper <webtkachdenis@gmail.com>
2026-05-15 13:56:45 +0300

    Update formatter.py

--- utils/formatter.py
+++ utils/formatter.py
@@ -20,6 +20,16 @@ def add_category_emoji(text):
             return f"{emoji} {text}"
     return f"📰 {text}"
 
+def normalize_link(url):
+    """Преобразует ссылки nitter.net в исходные x.com, остальные оставляет как есть."""
+    if not url:
+        return url
+    # Заменяем домен nitter.net на x.com
+    normalized = re.sub(r'https?://nitter\.net/', 'https://x.com/', url)
+    # Убираем якорь #m, если он есть (обычно добавляется Nitter)
+    normalized = re.sub(r'#m$', '', normalized)
+    return normalized
+
 def build_message(post, translate=False):
     original_text = post["text"]
     if translate:
@@ -29,9 +39,10 @@ def build_message(post, translate=False):
 
     text_with_emoji = add_category_emoji(ru_text)
 
-    # Добавляем ссылку на оригинал в конце
+    # Добавляем ссылку на оригинал, предварительно нормализовав её
     link = post.get("link")
     if link:
-        text_with_emoji += f'\n\n<a href="{link}">ОРИГИНАЛ</a>'
+        clean_link = normalize_link(link)
+        text_with_emoji += f'\n\n<a href="{clean_link}">ОРИГИНАЛ</a>'
 
     return text_with_emoji
